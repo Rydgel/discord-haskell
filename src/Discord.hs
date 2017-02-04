ee0491b0b94d4e145e40456ce0357eaee24fba94
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-02-05 00:12:47 +0100

    change http lib

--- src/Discord.hs
+++ src/Discord.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE RankNTypes            #-}
+{-# OPTIONS_GHC -fno-warn-orphans  #-}
 
 module Discord
     ( runDiscordT
@@ -37,13 +38,14 @@ import qualified Data.CaseInsensitive        as CI
 import qualified Data.Text                   as T
 import           Data.Typeable               (Typeable)
 import           Discord.Types
-import           Network.HTTP.Client         (HttpException (..), Manager,
-                                              Request (..), RequestBody (..),
-                                              Response (..), httpLbs,
-                                              newManager, parseUrlThrow)
-import           Network.HTTP.Client.TLS     (tlsManagerSettings)
-import           Network.HTTP.Types          (Header, methodPost)
-import           Network.HTTP.Types.Header   (ResponseHeaders)
+import           Network.HTTP.Client         (HttpException (..),
+                                              HttpExceptionContent (..), brRead,
+                                              responseHeaders, responseStatus)
+import           Network.HTTP.Req
+import           Network.HTTP.Types          (ResponseHeaders)
+
+instance MonadHttp IO where
+  handleHttpException = throwIO
 
 -- | The Discord monad, which supports exception and logging
 type DiscordT t m a = (MonadIO m, MonadLogger m, MonadBaseControl IO m) => ReaderT (DiscordConfig t) m a
@@ -63,10 +65,7 @@ runDiscordLogging :: MonadIO m => (DiscordConfig t -> DiscordT t (LoggingT IO) a
 runDiscordLogging config action =
   liftIO $ runStderrLoggingT $ runReaderT action config
 
-data DiscordConfig a = DiscordConfig
-  { dcApiKey  :: a
-  , dcManager :: Manager
-  }
+newtype DiscordConfig a = DiscordConfig { dcApiKey :: a }
 
 -- | Convenience method to ask ReaderT for the current API key.
 askApiKey :: MonadIO m => ReaderT (DiscordConfig t) m t
@@ -74,15 +73,10 @@ askApiKey = dcApiKey <$> ask
 
 -- | Creates a DiscordConfig with a new Manager.
 defaultDiscordConfig :: MonadIO m => t -> m (DiscordConfig t)
-defaultDiscordConfig apiKey = do
-  man <- liftIO $ newManager tlsManagerSettings
-  return $ DiscordConfig apiKey man
+defaultDiscordConfig apiKey = return $ DiscordConfig apiKey
 
 userAgent :: T.Text
 userAgent = "DiscordBot (https://github.com/Rydgel/discord-haskell, v0.1.0.0)"
-
-prepHeader :: (String, String) -> Header
-prepHeader (h, v) = (CI.mk $ BS.pack h, BS.pack v)
 
 -- | Perform a query to Discord. Should only be necessary to call directly for
 --   unimplemented methods. If you infer a return type of Value you will get the
@@ -104,27 +98,25 @@ query section apiMethod request = do
 query' :: FromJSON a => T.Text -> T.Text -> Value -> DiscordT t m a
 query' section apiMethod request = do
   config <- ask
-  initReq <- liftIO $ parseUrlThrow "https://discordapp.com/api/channels/1"
-  let req = initReq { requestBody = RequestBodyLBS $ encode request
-                    , method = methodPost
-                    }
-  logDebugN $ T.pack . show $ req
-  response <- catch (liftIO (httpLbs req $ dcManager config)) catchHttpException
-  logDebugN $ T.pack . show $ responseBody response
-  case decode $ responseBody response of
+  let url = https "discordapp.com" /: "api" /: "channels" /: "1"
+  r <- catch (liftIO (req POST url NoReqBody lbsResponse mempty)) catchHttpException
+  logDebugN $ T.pack . show $ responseBody r
+  case decode $ responseBody r of
     Just result -> return result
     Nothing -> throwIO $ OtherDiscordError (-1) "Could not parse result JSON from Discord"
 
--- | todo Add all cases possible if you want
-catchHttpException :: HttpException -> DiscordT t m a
-catchHttpException e@(StatusCodeException _ headers _) = do
-  logDebugN $ T.pack . show $ lookup "X-Response-Body-Start" headers
+catchHttpException :: Network.HTTP.Req.HttpException -> DiscordT t m a
+catchHttpException (VanillaHttpException (HttpExceptionRequest _ (StatusCodeException response body))) = do
+  logDebugN $ T.pack . show $ response
+  logDebugN $ T.pack . show $ body
+  let status = responseStatus response
+  logDebugN $ T.pack . show $ status
   let cantParseError = OtherDiscordError (-1) "Could not parse result JSON from Discord"
-  maybe (throwIO cantParseError) throwIO (decodeError headers)
+  maybe (throwIO cantParseError) throwIO (decodeError body)
 catchHttpException e = throwIO $ BadRequest 0 "Bad Request"
 
-decodeError :: ResponseHeaders -> Maybe DiscordError
-decodeError headers = fromStrict <$> lookup "X-Response-Body-Start" headers >>= decode
+decodeError :: BS.ByteString -> Maybe DiscordError
+decodeError body = decode (fromStrict body)
 
 data DiscordError = BadRequest Int T.Text
                   | OtherDiscordError Int T.Text
@@ -142,8 +134,6 @@ instance FromJSON DiscordError where
         0 -> BadRequest code message
         _ -> OtherDiscordError code message
   parseJSON _ = mzero
-
-
 
 -- move that
 -- | Creates Aeson objects after filtering to remove null values.
